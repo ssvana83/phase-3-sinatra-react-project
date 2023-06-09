f022d8da51c3f9c55eda2ddf9f25c25efacc7be0
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-06-09 13:22:37 -0400

    fixed with E

--- my-app-frontend/src/components/Player.js
+++ my-app-frontend/src/components/Player.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Player = ({players}) => {
+const Player = ({player}) => {
 
   // const playerList = player.map( p => <PlayerLink key={p.id} player={p} />)
 
-  console.log(players)
+  console.log(player)
   
   return (
     
     <div>
-      {/* <h3>{player.first_name} {player.last_name}</h3> */}
+      <h3>{player.first_name} {player.last_name}</h3>
       <button>Edit</button>
       <button>Delete</button>
       <br />
